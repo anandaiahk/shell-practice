81ced7bbb35e7b904fe1b5aa32a2275fb2fdbc6f
anand k <kanandaiah9@gmail.com>
2026-02-02 18:44:58 +0530

    modify

--- 06-variable.sh
+++ 06-variable.sh
@@ -2,6 +2,6 @@
 TIMESTAMP=$(date +%s)
 echo "the excute time is $TIMESTAMP"
 sleep 10
-ENDTIME=$(date *%s)
-TOTAL_TIME=$((ENDTIME-TIMESTAMP))
+ENDTIME=$(date +%s)
+TOTAL_TIME=$(($ENDTIME-$TIMESTAMP))
 echo "excute time is$TOTAL_TIME"
