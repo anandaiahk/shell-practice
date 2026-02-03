06f87ec2dcdb21596b447b8c9bd512d94be55ff2
anand k <kanandaiah9@gmail.com>
2026-02-03 12:02:04 +0530

    crerate loops

---
+++ 15-loops.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+if [ $USER_ID -ne 0 ]; then
+echo "please run the script in root aceess"
+exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]; then
+      echo "$2.......FAILURE" | tee -a $LOGS_FILE
+      exit 1
+     else
+       echo "$2........SUCESS" | tee -a $LOGS_FILE
+     fi
+
+}
+
+for package in $@  # to inupt nginx mysql nodejs
+do
+   dnf installed $package &>>$LOGS_FILE
+
+   if [ $? ne 0 ]; then
+         
+        echo "$package no installed,install now"
+    dnf install $package -y &>>$LOGS_FILE
+     VALIDATE $? "install $package"
+     else
+     echo "$package already installed....skipping"
+     fi
+done
